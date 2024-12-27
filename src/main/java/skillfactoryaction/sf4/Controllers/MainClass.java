b000217989a6a596f0d3cc1c0f17718031c16ca5
matthewencore <kirillkalmykov88@gmail.com>
2024-12-27 14:26:35 +0300

    залив

---
+++ src/main/java/skillfactoryaction/sf4/Controllers/MainClass.java
@@ -0,0 +1,22 @@
+package skillfactoryaction.sf4.Controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class MainClass {
+    @GetMapping(path = "/")
+    ResponseEntity<Map<String,String>> mainController(){
+        Map<String,String> stringMap = new HashMap<>();
+        stringMap.put("message","It's a simple app!");
+
+        return ResponseEntity.status(HttpStatus.OK).body(stringMap);
+    }
+
+}
---
+++ src/main/java/skillfactoryaction/sf4/Sf4Application.java
@@ -0,0 +1,13 @@
+package skillfactoryaction.sf4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sf4Application.class, args);
+	}
+
+}
